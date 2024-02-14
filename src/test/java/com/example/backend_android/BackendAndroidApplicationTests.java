e86edc024a9f5efd45edbeda4c49d64f7092b7d4
Baitur Tashbaev <tashbaevvb@gmail.com>
2024-02-14 21:33:04 +0600

    inital

---
+++ src/test/java/com/example/backend_android/BackendAndroidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_android;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAndroidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
